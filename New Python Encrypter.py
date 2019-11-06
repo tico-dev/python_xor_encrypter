6c86eef5333cd1665ee53b56b48c6e2602c10873
Gustavo-Herreira <57445748+Gustavo-Herreira@users.noreply.github.com>
2019-11-06 10:48:32 -0300

    New Python Encrypter
     This code is an optimization (and reduction) of an old code created by me.
     While I was coding with 3 other friends, Luiz was studying about the XOR operator to help us with the code.
     The algorithm was improved by Luiz [github.com/luiz-diniz], he introduced me to the XOR "^" operator syntax, which reduced the code by 80%, considering that I didn't know this operator.

---
+++ New Python Encrypter.py
@@ -0,0 +1,45 @@
+from random import randint
+
+def key():
+    chave = []
+    for c in range(50):
+        chave.append(chr(randint(33, 126)))
+    print('\nSua chave:\n' + ''.join(chave) + '\n')
+    return chave
+
+
+def encrypt():
+    criptografado = []
+    for c in range(len(mensagem)):
+        criptografado.append(chr(ord(mensagem[c]) ^ (ord(_chave[c]))))
+    print('Sua mensagem criptografada:\n')
+    print(''.join(criptografado))
+    print('\n')
+
+
+while True:
+    decisao = input(
+        '[1] Digite "1" para Criptografar;\n'
+        '[2] Digite "2" para Descriptografar;\n'
+        '[3] Digite "3" para Sair.\n'
+        '[User]: ')
+
+
+    if decisao == '1':
+        print('---------------Criptografar---------------')
+        _chave = key()
+        mensagem = list(input('Digite sua mensagem:\n'))
+        encrypt()
+
+    if decisao == '2':
+        print('-------------Descriptografar-------------')
+        mensagem = list(input('Digite sua mensagem criptografada:\n'))
+        _chave = list(input('Digite sua chave:\n'))
+        while len(_chave) < len(mensagem):
+            print('A chave deve ser maior do que a mensagem\n')
+            _chave = list(input('Digite sua chave:\n'))
+        encrypt()
+
+    if decisao == '3':
+        print('Programa fechado.')
+        break
